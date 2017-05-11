747c2835f3270217904a58dd76874c4371cdee81
howardjmn <semiles@msn.com>
2017-05-11 16:49:43 -0500

    Project Start

--- js/perfmatters.js
+++ js/perfmatters.js
@@ -1,7 +1,8 @@
 // Measuring the Critical Rendering Path with Navigation Timing
 // https://developers.google.com/web/fundamentals/performance/critical-rendering-path/measure-crp
 
-function logCRP() {
+function logCRP()
+{
   var t = window.performance.timing,
     dcl = t.domContentLoadedEventStart - t.domLoading,
     complete = t.domComplete - t.domLoading;
@@ -9,6 +10,7 @@ function logCRP() {
   stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
 }
 
-window.addEventListener("load", function(event) {
+window.addEventListener("load", function(event)
+{
   logCRP();
 });
